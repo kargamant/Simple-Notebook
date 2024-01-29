f1fbddd09770e343e714683390fde69b2c022f14
kargamant <firexholms@gmail.com>
2024-01-29 13:35:20 +0300

    Now chosen files are opened in different tabs, but not viewed correctly

--- notes/mainwindow.cpp
+++ notes/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QFileDialog>
+#include <QTextBrowser>
 #include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -17,16 +18,24 @@ void MainWindow::open()
     std::cout<<"chosen files:"<<std::endl;
     for(auto itr: files)
     {
+        QString filename;
         for(QChar* qch=itr.data(); qch-itr.data()<itr.length(); ++qch)
         {
             std::cout<<qch->toLatin1();
+            //if(qch->toLatin1()=='/') filename+="\\";
+            filename+=qch->toLatin1();
         }
         std::cout<<std::endl;
+
+        QTextBrowser* qb=new QTextBrowser(this); //set to textedit
+
+        qb->setSource(QUrl::fromLocalFile(filename));
+        ui->FileTabs->addTab(qb, filename);
     }
 }
 
 MainWindow::~MainWindow()
 {
-    //for(int i=0; i<ui->FileTabs->count(); i++) delete ui->FileTabs->widget(i);
+    for(int i=0; i<ui->FileTabs->count(); i++) delete ui->FileTabs->widget(i);
     delete ui;
 }
