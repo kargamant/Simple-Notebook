6409e1ece1bfd0f57dd7008c2de1a5a0882c4183
kargamant <firexholms@gmail.com>
2024-01-29 12:35:37 +0300

    Made file chosage

--- notes/mainwindow.cpp
+++ notes/mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFileDialog>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->openButton, &QPushButton::clicked, this, &MainWindow::open);
+}
+
+void MainWindow::open()
+{
+    QStringList files=QFileDialog::getOpenFileNames(this, "Choose a file", "C:\\Users\\Honor", "*.txt *.cpp *.c *.py");
+    std::cout<<"chosen files:"<<std::endl;
+    for(auto itr: files)
+    {
+        for(QChar* qch=itr.data(); qch-itr.data()<itr.length(); ++qch)
+        {
+            std::cout<<qch->toLatin1();
+        }
+        std::cout<<std::endl;
+    }
 }
 
 MainWindow::~MainWindow()
 {
+    //for(int i=0; i<ui->FileTabs->count(); i++) delete ui->FileTabs->widget(i);
     delete ui;
 }
